4b4c4cecd96bebf67d61140887e61107ba3f518e
Michael Smith <chukobyte@gmail.com>
2020-07-05 21:04:09 -0400

    Cleaning up commented code in Font Handler.

--- src/core/handlers/FontHandler.cpp
+++ src/core/handlers/FontHandler.cpp
@@ -1,16 +1,6 @@
 #include "FontHandler.h"
 #include "../Game.h"
 
-/*
-TTF_Font* FontHandler::LoadFont(const char* fileName, int fontSize) {
-    TTF_Font* newFont = TTF_OpenFont(fileName, fontSize);
-    if(!newFont) {
-    	printf("Error loading font at: %s\n", TTF_GetError());
-    }
-    return newFont;
-}
-*/
-
 Font* FontHandler::LoadFont(const char* fileName, int size) {
     Font* font = new Font(Game::textRenderer->freeTypeLibrary, fileName, size);
     if(!font->IsValid()) {
@@ -18,12 +8,6 @@ Font* FontHandler::LoadFont(const char* fileName, int size) {
     }
     return font;
 }
-
-/*
-void FontHandler::Draw(SDL_Texture* texture, SDL_Rect position) {
-    SDL_RenderCopy(Game::renderer, texture, NULL, &position);
-}
-*/
 
 void FontHandler::Draw(Font* font, std::string text, float x, float y, float scale, glm::vec3 color) {
     Game::textRenderer->Draw(font, text, x, y, scale, color);
